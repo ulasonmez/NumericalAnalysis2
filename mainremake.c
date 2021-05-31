12ac98f277f333b8736b17596fe00e06ed296366
ulasonmez <ulasonmez@outlook.com>
2021-05-31 04:32:18 +0300

    Update mainremake.c

--- mainremake.c
+++ mainremake.c
@@ -3,7 +3,8 @@
 #include <math.h>
 
 float fonksiyonSonuc(float xDegeri){
-	return exp(-0.5*(xDegeri))-a-0.2;
+	float e = exp(1);
+	return pow(e,-0.5*(xDegeri))-xDegeri-0.2;
 }
 double ikiyeBolmeHata(double son,double ilk,double iterasyon){
 	return ((son-ilk)/pow(2,iterasyon));
@@ -18,8 +19,9 @@ float dogrusalInterpolasyonKok(float ilk,float son){
 	float yeniKok = (ilk*(fonksiyonSonuc(son)) - son*(fonksiyonSonuc(ilk))) / (fonksiyonSonuc(son) - fonksiyonSonuc(ilk));
 	return yeniKok;
 }
-float fonksiyonTurevSonuc(float x){
-	return ((-0.5*(x))*exp((-0.5*(x))-1))-1;
+float fonksiyonTurevSonuc(float xDegeri){
+	float e = exp(1);
+	return (-0.5*(xDegeri))*pow(e,(-0.5*(xDegeri))-1)-1;
 }
 int main(int argc, char *argv[]) {
 	int iterasyon,j,ibKontrol=0,ntKontrol=0,diKontrol=0;
@@ -61,23 +63,23 @@ int main(int argc, char *argv[]) {
 			newtonThompsonBaslangic = newtonThompsonYeni;
 			dogrusalInterOrta = dogrusalInterpolasyonKok(dogrusalInterIlk,dogrusalInterSon);
 			if(fonksiyonSonuc(dogrusalInterIlk)*fonksiyonSonuc(dogrusalInterSon)<0){
-					printf("\t\t%f\t%f\n",dogrusalInterOrta,dogrusalInterpolasyonHata(dogrusalInterSon,dogrusalInterOrta));
-					if(dogrusalInterpolasyonHata(dogrusalInterSon,dogrusalInterOrta)<0.02 && diKontrol==0){
-						diKontrol=iterasyon;
-					}
-					if(fonksiyonSonuc(dogrusalInterIlk)*fonksiyonSonuc(dogrusalInterOrta)<0){
-						dogrusalInterSon = dogrusalInterOrta;
-					}
-					else if(fonksiyonSonuc(dogrusalInterSon)*fonksiyonSonuc(dogrusalInterOrta)<0){
-						dogrusalInterIlk = dogrusalInterOrta;
-					}
-					dogrusalInterOrta = dogrusalInterpolasyonKok(dogrusalInterIlk,dogrusalInterSon);
+				printf("\t\t%f\t%f\n",dogrusalInterOrta,dogrusalInterpolasyonHata(dogrusalInterSon,dogrusalInterOrta));
+				if(dogrusalInterpolasyonHata(dogrusalInterSon,dogrusalInterOrta)<0.02 && diKontrol==0){
+					diKontrol=iterasyon;
+				}
+				if(fonksiyonSonuc(dogrusalInterIlk)*fonksiyonSonuc(dogrusalInterOrta)<0){
+					dogrusalInterSon = dogrusalInterOrta;
+				}
+				else if(fonksiyonSonuc(dogrusalInterSon)*fonksiyonSonuc(dogrusalInterOrta)<0){
+					dogrusalInterIlk = dogrusalInterOrta;
+				}
+				dogrusalInterOrta = dogrusalInterpolasyonKok(dogrusalInterIlk,dogrusalInterSon);
 			}
 		}
 	}
-		printf("\n\nIkiye bolme metodu ile 0.02 den daha kucuk hata degerine %d. adimda ulasilmistir.\n",ibKontrol);
-		printf("Newton Thompson metodu ile 0.02 den daha kucuk hata degerine %d. adimda ulasilmistir.\n",ntKontrol);
-		printf("Dogrusal Interpolasyon metodu ile 0.02 den daha kucuk hata degerine %d. adimda ulasilmistir.\n",diKontrol);
+		printf("\n\n1. Metot = Ikiye bolme metodu ile 0.02 den daha kucuk hata degerine %d. adimda ulasilmistir.\n",ibKontrol);
+		printf("2. Metot = Newton Thompson metodu ile 0.02 den daha kucuk hata degerine %d. adimda ulasilmistir.\n",ntKontrol);
+		printf("3. Metot = Dogrusal Interpolasyon metodu ile 0.02 den daha kucuk hata degerine %d. adimda ulasilmistir.\n",diKontrol);
 
 	return 0;
 }
